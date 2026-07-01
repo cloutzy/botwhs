aa0af8aa77c129bb2d9b02f05788af4e3a7075c7
cloutzy <69744783+cloutzy@users.noreply.github.com>
2026-07-01 10:17:39 -0600

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,10 +1,13 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
+const path = require('path'); // Agrega esta línea arriba
 
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
+        // Esta línea le dice al bot que busque Chrome dentro de la carpeta del proyecto:
+        executablePath: path.join(__dirname, '.local-chromium', 'linux-146.0.7680.31', 'chrome-linux', 'chrome'), 
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
@@ -17,7 +20,6 @@ const client = new Client({
         ]
     }
 });
-
 // Genera el código QR en la consola para escanearlo con el celular
 client.on('qr', (qr) => {
     qrcode.generate(qr, { small: true });
